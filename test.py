d859dca5a589124c996ddc8ac854b6a862ed00d3
Owen-I <owenwolfz10@gmail.com>
2025-12-06 13:41:40 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import pygame 
+import random
 
 BLACK = (0, 0 ,0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-screenHeight = 300
-screenWidth = 300
+screenWidth = 1000
+screenHeight = 800
+
 #screen
 
 background_colour = (BLACK)
@@ -13,14 +15,27 @@ screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('GAME Jam')
 screen.fill(background_colour)
 
+def randomInt(x, y):
+   return random.randint(x, y)
+
+
+class person:
+  def __init__ (self, name, rect, speed):
+    self.name = name
+    self.rect = rect
+    self.speed = speed
+
+enemy = []
+
+for i in range ((screenHeight) // 100):
+    enemy.append(person(("enemy" + str(i)), (pygame.Rect(1, i*100, randomInt(50,100), randomInt(50,100))), randomINt(1,5)))
+
 
 running = True
 
 while running:
-    
-    for i in range ((screenHeight) // 50):
-        pygame.draw.rect(screen, RED, (1 , 50*i , screenWidth, 1))
-
+    for i in range (len(enemy)):
+        pygame.draw.rect(screen, RED, enemy[i].rect)
     pygame.display.flip()
     
     for event in pygame.event.get():    
