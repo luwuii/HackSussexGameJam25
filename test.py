bab5e7ad34af06cbc6eb3f8fd4caa4c96eb4e304
Louis Moore <97617699+luwuii@users.noreply.github.com>
2025-12-06 16:21:28 +0000

    Update test.py

--- test.py
+++ test.py
@@ -7,9 +7,43 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+WHITE = (255,255,255)
+GREY = (100,100,100)
 
-screenWidth = 1000
-screenHeight = 800
+screenWidth = 1280
+screenHeight = 720
+
+#display the shop menu
+def display_shop():
+    if shop_open:
+        #shop window
+        pygame.draw.rect(screen, GREY, (200, 100, 880, 520))
+        #shop border
+        pygame.draw.rect(screen, BLACK, (200, 100, 880, 520), 4)
+        
+        draw_text("Ye olde shoppe" ,magicfont, BLACK, 220,120)
+        draw_text("Buy potions and spells to defend yourself against the evil malwares" ,magicfont, BLACK, 220,180)
+
+shop_open = False
+
+#draw shop button
+def draw_shopButton():
+    # button size
+    w, h = 120, 40
+
+    # top-left corner (10px margin)
+    x = 10
+    y = 10
+
+    # draw button
+    pygame.draw.rect(screen, WHITE, (x, y, w, h))
+    
+    draw_text("SHOP" ,magicfont, BLACK,(x + w // 2)-35,(y + h // 2-15))
+
+    
+    return pygame.Rect(x, y, w, h)   # return button rect for clicking
+
+
 
 #screen
 def draw_text(text,font,text_col,x,y):
@@ -21,8 +55,10 @@ pygame.display.set_caption('GAME Jam')
 screen.fill(BLACK)
 
 
+#DEFINING FONTS
 pygame.font.init()
 font = pygame.font.SysFont('arial',30)
+magicfont = pygame.font.Font('Heraldic Shadows.otf',30)
 
 clock = pygame.time.Clock()
 
@@ -55,7 +91,9 @@ running = True
 coins = 0
 while running:
     clock.tick(60)
-    screen.fill(BLACK)          
+    screen.fill(BLACK)
+
+
 
     draw_text(("coins " + str(coins)), font ,YELLOW ,900, 10)   
 
@@ -70,12 +108,25 @@ while running:
         if enemy[i].rect.x > 100 + screenWidth:
             enemy[i].rect.x = - 100
 
-    pygame.display.flip()
+    display_shop()
+    shop_rect = draw_shopButton()
+    
+    
 
     for event in pygame.event.get():        
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+
+
+#-----------------------SHOP------------------------
+    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if shop_rect.collidepoint(event.pos):
+                shop_open = not shop_open
+            pygame.time.wait(100)
+#---------------------------------------------------
+
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             for e in enemy:
                 if e.rect.collidepoint(pos):
@@ -84,3 +135,7 @@ while running:
                     if e.health == 0:
                         e.rect.x = -100
                         e.health = randomInt(1,5)
+
+
+
+    pygame.display.flip()
