83c27886b4e604000d7b9d807f0b0862a91b6e5c
Owen-I <owenwolfz10@gmail.com>
2025-12-06 12:50:57 +0000

    henlo

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-print("meow")
+import pygame 
+
+BLACK = (0, 0 ,0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+screenHeight = 300
+screenWidth = 300
+#screen
+
+background_colour = (BLACK)
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption('GAME Jam')
+screen.fill(background_colour)
+
+
+running = True
+
+while running:
+    
+    for i in range ((screenHeight) // 50):
+        pygame.draw.rect(screen, RED, (1 , 50*i , screenWidth, 1))
+
+    pygame.display.flip()
+    
+    for event in pygame.event.get():    
+        # Check for QUIT event      
+        if event.type == pygame.QUIT:
+            running = False
