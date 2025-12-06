fcdbd59aa252a07920fe81962623099f5270e01b
Owen-I <owenwolfz10@gmail.com>
2025-12-06 13:54:53 +0000

    Update test.py

--- test.py
+++ test.py
@@ -10,10 +10,11 @@ screenHeight = 800
 
 #screen
 
-background_colour = (BLACK)
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('GAME Jam')
-screen.fill(background_colour)
+screen.fill(BLACK)
+
+clock = pygame.time.Clock()
 
 def randomInt(x, y):
    return random.randint(x, y)
@@ -28,16 +29,22 @@ class person:
 enemy = []
 
 for i in range ((screenHeight) // 100):
-    enemy.append(person(("enemy" + str(i)), (pygame.Rect(1, i*100, randomInt(50,100), randomInt(50,100))), randomINt(1,5)))
+    enemy.append(person(("enemy" + str(i)), (pygame.Rect(1, i*100, randomInt(50,100), randomInt(50,100))), randomInt(1,5)))
 
 
 running = True
 
 while running:
+    clock.tick(60)
     for i in range (len(enemy)):
         pygame.draw.rect(screen, RED, enemy[i].rect)
+    for i in range (len(enemy)):
+        enemy[i].rect.x += 1
+        print(enemy[1].rect.x)
+
     pygame.display.flip()
-    
+
+    screen.fill(BLACK)
     for event in pygame.event.get():    
         # Check for QUIT event      
         if event.type == pygame.QUIT:
